f1cd10ea332ced568d702dd7501d7cbcdc8cbef3
Eilon Krauthammer <mitleyber@gmail.com>
2025-05-29 11:12:47 +0300

    Initial Commit

---
+++ LocalizerExample/LocalizerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizerExampleApp.swift
+//  LocalizerExample
+//
+//  Created by Eilon Krauthammer on 29/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct LocalizerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
