f448034e1373dddb4d3f0a9c9e8fa28165747da2
Eilon Krauthammer <mitleyber@gmail.com>
2025-05-29 16:33:25 +0300

    Add ContentModel class for localized string handling

---
+++ LocalizerExample/ContentModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentViewModel.swift
+//  LocalizerExample
+//
+//  Created by Eilon Krauthammer on 29/05/2025.
+//
+
+import Foundation
+
+class ContentModel: Observable {
+    let text = "Test from ContentModel"
+}
