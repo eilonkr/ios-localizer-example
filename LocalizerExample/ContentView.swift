4193cefc28f7741645c4fda349106cc642ac7e56
Eilon Krauthammer <mitleyber@gmail.com>
2025-05-29 13:32:27 +0300

    String update test

--- LocalizerExample/ContentView.swift
+++ LocalizerExample/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
             
             Text("Today is \(Date().formatted(date: .abbreviated, time: .omitted))")
             
-            Text("Change string test")
+            Text("Updated string here")
             
             Text("New test string! Hi :)")
             
